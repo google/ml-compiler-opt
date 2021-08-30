fa356bf31e0418267ea33acf5eefafeaf8203c18
Mircea Trofin <mtrofin@google.com>
2021-08-30 09:51:08 -0700

    Fix tf c api package path

--- buildbot/buildbot_init.sh
+++ buildbot/buildbot_init.sh
@@ -131,7 +131,7 @@ mkdir /tmp/tensorflow
 export TENSORFLOW_API_PATH=/tmp/tensorflow
 wget --quiet 	https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-cpu-linux-x86_64-2.6.0.tar.gz \
   || on_error "failed to download tensorflow C library"
-tar xfz libtensorflow-cpu-linux-x86_64-2.5.0.tar.gz -C "${TENSORFLOW_API_PATH}" || echo "failed to unarchive tensorflow C library"
+tar xfz libtensorflow-cpu-linux-x86_64-2.6.0.tar.gz -C "${TENSORFLOW_API_PATH}" || echo "failed to unarchive tensorflow C library"
 
 if [ -f "${TENSORFLOW_API_PATH}/lib/libtensorflow.so" ]
 then
