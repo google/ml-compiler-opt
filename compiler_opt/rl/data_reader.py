87104f3cf9fac97902cb15a8a6f7b75ddf97ad39
Northbadge <njx@google.com>
2022-06-29 14:22:22 -0700

    Add caching to input pipeline (#34)
    Caching the results pre-shuffling prevents TF from recomputing the pre-shuffle batches from scratch repeatedly.
    A speedup of about >5x is expected for the input pipeline, and similarly for the training cycle times as well.

--- compiler_opt/rl/data_reader.py
+++ compiler_opt/rl/data_reader.py
@@ -165,15 +165,16 @@ def create_sequence_example_dataset_fn(
   def _sequence_example_dataset_fn(sequence_examples):
     # Data collector returns empty strings for corner cases, filter them out
     # here.
-    dataset = tf.data.Dataset.from_tensor_slices(sequence_examples).filter(
-        lambda string: tf.strings.length(string) > 0).map(parser_fn).filter(
-            lambda traj: tf.size(traj.reward) > 2)
-    dataset = (
-        dataset.unbatch().batch(
-            train_sequence_length,
-            drop_remainder=True).shuffle(trajectory_shuffle_buffer_size).batch(
-                batch_size,
-                drop_remainder=True).prefetch(tf.data.experimental.AUTOTUNE))
+    dataset = (tf.data.Dataset.from_tensor_slices(sequence_examples)
+              .filter(lambda string: tf.strings.length(string) > 0)
+              .map(parser_fn)
+              .filter(lambda traj: tf.size(traj.reward) > 2)
+              .unbatch()
+              .batch(train_sequence_length, drop_remainder=True)
+              .cache()
+              .shuffle(trajectory_shuffle_buffer_size)
+              .batch(batch_size, drop_remainder=True)
+              )
     return dataset
 
   return _sequence_example_dataset_fn
