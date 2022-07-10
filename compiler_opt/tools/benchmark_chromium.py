2d5b9e073b91f728c50b8fa9fc589b0d71c2dcde
Aiden Grossman <agrossman154@yahoo.com>
2022-07-10 19:11:42 +0000

    Linted benchmark_chromium

--- compiler_opt/tools/benchmark_chromium.py
+++ compiler_opt/tools/benchmark_chromium.py
@@ -36,9 +36,10 @@ PYTHONPATH=$PYTHONPATH:. python3 ./compiler_opt/tools/benchmark_chromium.py \
   --perf_counters=mem_uops_retired.all_stores
 
 Note that --perf_counters can be defined multiple times to grab more than one
-performance counter. Also note that the chromium_build_path is a relative directory.
-It is relative to the chromium source dir and specified this way as there appears
-to be problems building chromium outside of the source directory.
+performance counter. Also note that the chromium_build_path is a relative
+directory. It is relative to the chromium source dir and specified this way
+as there appears to be problems building chromium outside of the source
+directory.
 """
 
 import os
@@ -60,49 +61,79 @@ default_test_descriptions = [
   "./compiler_opt/tools/chromium_test_descriptions/components_perftests.json"
 ]
 
-flags.DEFINE_multi_string("test_description", default_test_descriptions, "(Can be defined multiple times) A path to a test description JSON file containing the test executable and the tests to run")
-flags.DEFINE_boolean("compile_tests", True, "Whether or not to compile the tests from scratch")
-flags.DEFINE_enum("advisor", None, ["release", "default"], "The advisor to use when compiling chromium")
-flags.DEFINE_string("chromium_src_path", "/chromium/src", "The path to the chromium source")
-flags.DEFINE_string("depot_tools_path", "/depot_tools", "The path to your depot tools checkout")
-flags.DEFINE_string("llvm_build_path", "/llvm-build", "The path to your llvm build")
-flags.DEFINE_boolean("compile_llvm", True, "whether or not to compile llvm using the new model")
-flags.DEFINE_boolean("llvm_use_incremental", True, "whether or not to use an incremental build while compiling llvm")
-flags.DEFINE_string("llvm_source_path", "/llvm-project", "The root path of your local llvm-project checkout")
-flags.DEFINE_string("model_path", "", "The path to the model to use when compiling llvm")
-flags.DEFINE_string("tensorflow_c_lib_path", "/tmp/tensorflow", "The path to an extracted copy of the tensorflow c library")
-flags.DEFINE_string("chromium_build_path", "./out/Release", "The chromium build path, relative to the chromium source directory")
-flags.DEFINE_string("output_file", "output.json", "The path to the output file (in JSON format)")
-flags.DEFINE_integer("num_threads", 1, "The number of threads to use when running benchmarks. Should be used with caution")
-flags.DEFINE_multi_string("perf_counters", ["mem_uops_retired.all_loads","mem_uops_retired.all_stores"], "The performance counters to use")
+flags.DEFINE_multi_string("test_description", default_test_descriptions,
+                          """(Can be defined multiple times) A path to a test
+                          description JSON file containing the test executable
+                          and the tests to run""")
+flags.DEFINE_boolean("compile_tests", True,
+                     "Whether or not to compile the tests from scratch")
+flags.DEFINE_enum("advisor", None, ["release", "default"],
+                  "The advisor to use when compiling chromium")
+flags.DEFINE_string("chromium_src_path", "/chromium/src",
+                    "The path to the chromium source")
+flags.DEFINE_string("depot_tools_path", "/depot_tools",
+                    "The path to your depot tools checkout")
+flags.DEFINE_string("llvm_build_path", "/llvm-build",
+                    "The path to your llvm build")
+flags.DEFINE_boolean("compile_llvm", True,
+                     "whether or not to compile llvm using the new model")
+flags.DEFINE_boolean("llvm_use_incremental", True,
+                     """whether or not to use an incremental build while
+                     compiling llvm""")
+flags.DEFINE_string("llvm_source_path", "/llvm-project",
+                    "The root path of your local llvm-project checkout")
+flags.DEFINE_string("model_path", "",
+                    "The path to the model to use when compiling llvm")
+flags.DEFINE_string("tensorflow_c_lib_path", "/tmp/tensorflow",
+                    "The path to an extracted copy of the tensorflow c library")
+flags.DEFINE_string("chromium_build_path", "./out/Release",
+                    """The chromium build path, relative to the chromium source
+                    directory""")
+flags.DEFINE_string("output_file", "output.json",
+                    "The path to the output file (in JSON format)")
+flags.DEFINE_integer("num_threads", 1,
+                     """The number of threads to use when running benchmarks.
+                     Should be used with caution""")
+flags.DEFINE_multi_string("perf_counters",
+                          ["mem_uops_retired.all_loads",
+                          "mem_uops_retired.all_stores"],
+                          "The performance counters to use")
 
-def build_chromium_tests(regalloc_advisor, chromium_build_path, chromium_source_path, depot_tools_path, llvm_build_path, tests_to_build):
+def build_chromium_tests(regalloc_advisor,
+                         chromium_build_path,
+                         chromium_source_path,
+                         depot_tools_path,
+                         llvm_build_path,
+                         tests_to_build):
   """Builds the chromium test suite
 
-  This function will build the specified chromium test targets using the specified
+  This function will build the specified chromium tests using the specified
   regalloc advisor. This function configures some default build options using gn
   as shown below in the gn_args list, and then builds the needed targets using
   autoninja.
 
   Args:
     regalloc_advisor: The regalloc advisor to use when compiling
-    chromium_build_path: The path (relative to the chromium source dir) to use for building chromium
+    chromium_build_path: The path (relative to the chromium source dir) to use
+      for building chromium
     chromium_source_path: The path to the chromium source
     depot_tools_path: The path to the root of your depot tools checkout
     llvm_build_path: The path to the root of the direcotry where llvm was built
     tests_to_build: An array of test targets that are to be built
   """
-  chromium_absolute_build_path = os.path.join(chromium_source_path, chromium_build_path)
+  chromium_absolute_build_path = os.path.join(chromium_source_path,
+                                              chromium_build_path)
   if os.path.exists(chromium_absolute_build_path):
     shutil.rmtree(chromium_absolute_build_path)
-    
+
   new_environment = os.environ.copy()
   new_environment["PATH"] += ":" + depot_tools_path
   new_environment["CC"] = os.path.join(llvm_build_path, "./bin/clang")
   new_environment["CXX"] = os.path.join(llvm_build_path, "./bin/clang++")
   new_environment["AR"] = os.path.join(llvm_build_path, "./bin/llvm-ar")
   new_environment["NM"] = os.path.join(llvm_build_path, "./bin/llvm-nm")
-  new_environment["CPPFLAGS"] = "-mllvm -regalloc-enable-advisor={advisor}".format(advisor=regalloc_advisor)
+  new_environment["CPPFLAGS"] = \
+    f"""-mllvm-regalloc-enable-advisor={regalloc_advisor}"""
 
   gn_args = [
     "is_official_build=true",
@@ -123,18 +154,23 @@ def build_chromium_tests(regalloc_advisor, chromium_build_path, chromium_source_
   gn_args_string += '"'
 
   gn_config_command = "gn gen " + chromium_build_path + " " + gn_args_string
-
-  print(gn_config_command)
-
-  gn_config_process = subprocess.Popen(gn_config_command, env=new_environment, cwd=chromium_source_path, shell=True)
-  gn_config_process.wait()
+  with subprocess.Popen(gn_config_command,
+                        env=new_environment,
+                        cwd=chromium_source_path,
+                        shell=True) as gn_config_process:
+    gn_config_process.wait()
 
   ninja_compile_command = ["autoninja", "-C", chromium_build_path]
   ninja_compile_command.extend(tests_to_build)
-  ninja_compile_process = subprocess.Popen(ninja_compile_command, env=new_environment, cwd=chromium_source_path)
-  ninja_compile_process.wait()
+  with subprocess.Popen(ninja_compile_command,
+                        env=new_environment,
+                        cwd=chromium_source_path) as ninja_compile_process:
+    ninja_compile_process.wait()
 
-def run_tests(tests_to_run, chromium_absolute_build_path, num_threads, perf_counters):
+def run_tests(tests_to_run,
+              chromium_absolute_build_path,
+              num_threads,
+              perf_counters):
   """A utility to run a set of chromium tests
 
   This function takes in a list of test descriptions containing the
@@ -146,36 +182,54 @@ def run_tests(tests_to_run, chromium_absolute_build_path, num_threads, perf_coun
 
   Args:
     tests_to_run: A list of python dictionaries containing the test descriptions
-    chromium_absolute_build_path: The absolute build path to the chromium build dir
+    chromium_absolute_build_path: The absolute build path to the chromium
+      build dir
     num_threads: The number of threads to use when running tests
     perf_counters: A list of perf compatible performance counters
   """
   test_data = {}
   for test in tests_to_run:
-    executable_path = os.path.join(chromium_absolute_build_path, test["executable"])
-    test_data.update(gtest_executable_utils.run_test_suite(test, executable_path, perf_counters, num_threads))
+    executable_path = os.path.join(chromium_absolute_build_path,
+                                   test["executable"])
+    test_data.update(gtest_executable_utils.run_test_suite(test,
+                                                           executable_path,
+                                                           perf_counters,
+                                                           num_threads))
   return test_data
 
-def main(argv):
+def main():
   test_descriptions = []
   for test_description in FLAGS.test_description:
-    with open(test_description) as test_description_file:
+    with open(test_description, encoding="UTF-8") as test_description_file:
       print(test_description)
       test_descriptions.append(json.load(test_description_file))
   test_executables = []
   for test_description in test_descriptions:
     test_executables.append(test_description["executable"])
-  
+
   if FLAGS.compile_llvm:
-    benchmarking_utils.build_llvm(FLAGS.model_path, FLAGS.llvm_use_incremental, FLAGS.llvm_build_path, FLAGS.llvm_source_path, FLAGS.tensorflow_c_lib_path)
+    benchmarking_utils.build_llvm(FLAGS.model_path,
+                                  FLAGS.llvm_use_incremental,
+                                  FLAGS.llvm_build_path,
+                                  FLAGS.llvm_source_path,
+                                  FLAGS.tensorflow_c_lib_path)
 
   if FLAGS.compile_tests:
-    build_chromium_tests(FLAGS.advisor, FLAGS.chromium_build_path, FLAGS.chromium_src_path, FLAGS.depot_tools_oath, FLAGS.llvm_build_path, test_executables)
+    build_chromium_tests(FLAGS.advisor,
+                         FLAGS.chromium_build_path,
+                         FLAGS.chromium_src_path,
+                         FLAGS.depot_tools_oath,
+                         FLAGS.llvm_build_path,
+                         test_executables)
 
-  chromium_absolute_build_path = os.path.join(FLAGS.chromium_src_path, FLAGS.chromium_build_path)
-  test_data = run_tests(test_descriptions, chromium_absolute_build_path, FLAGS.num_threads, FLAGS.perf_counters)
+  chromium_absolute_build_path = os.path.join(FLAGS.chromium_src_path,
+                                              FLAGS.chromium_build_path)
+  test_data = run_tests(test_descriptions,
+                        chromium_absolute_build_path,
+                        FLAGS.num_threads,
+                        FLAGS.perf_counters)
 
-  with open(FLAGS.output_file, "w") as output_file:
+  with open(FLAGS.output_file, "w", encoding="UTF-8") as output_file:
     output_file.write(json.dumps(test_data, indent=4))
 
 if __name__ == "__main__":
