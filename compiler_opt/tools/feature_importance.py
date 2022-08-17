36eb6bafe4d9aa1b6c5a1770de960f5c241ae6f8
Aiden Grossman <agrossman154@yahoo.com>
2022-08-17 07:44:29 +0000

    Probably finished up feature importance tool for grabbing shap values

--- compiler_opt/tools/feature_importance.py
+++ compiler_opt/tools/feature_importance.py
@@ -34,6 +34,12 @@ _DATA_PATH = flags.DEFINE_multi_string(
 _MODEL_PATH = flags.DEFINE_string(
   'model_path', '',
   'Path to the model to explain')
+_OUTPUT_FILE = flags.DEFINE_string(
+  'output_file', '',
+  'The path to the output file containing the SHAP values')
+_NUM_EXAMPLES = flags.DEFINE_integer(
+  'num_examples', '',
+  'The number of examples to process from the trace')
 _GIN_FILES = flags.DEFINE_multi_string(
     'gin_files', [], 'List of paths to gin configuration files.')
 _GIN_BINDINGS = flags.DEFINE_multi_string(
@@ -43,24 +49,48 @@ _GIN_BINDINGS = flags.DEFINE_multi_string(
 def get_input_signature(example_input):
   input_signature = {}
   for input in example_input:
-    input_signature[input] = tf.shape(example_input[input]).numpy()
+    input_signature[input] = (tf.shape(example_input[input]).numpy(), example_input[input].dtype)
   return input_signature
 
 def get_signature_total_size(input_signature):
   total_size = 0
   for input in input_signature:
-    total_size += numpy.prod(input_signature[input])
+    total_size += numpy.prod(input_signature[input][0])
   return total_size
 
 def pack_flat_array_into_input(flat_array, signature_spec):
   output_input_dict = {}
   current_index = 0
   for needed_input in signature_spec:
-    part_size = numpy.prod(signature_spec[needed_input])
-    needed_subset = flat_array[current_index:part_size]
+    part_size = numpy.prod(signature_spec[needed_input][0])
+    needed_subset = flat_array[current_index:current_index + part_size]
     current_index += part_size
-    output_input_dict[needed_input] = tf.constant(needed_subset)
+    output_input_dict[needed_input] = tf.cast(tf.constant(needed_subset, shape=signature_spec[needed_input][0]), dtype=signature_spec[needed_input][1])
   return output_input_dict
+
+def flatten_input(input, array_size):
+  output_array = numpy.empty(array_size)
+  input_index = 0
+  for input_key in input:
+    current_size = tf.size(input[input_key])
+    end_index = input_index + current_size
+    output_array[input_index:end_index] = input[input_key].numpy().astype(numpy.float32)
+    input_index += current_size
+  return output_array
+
+def process_raw_trajectory(raw_trajectory):
+  observation = raw_trajectory.observation
+  observation.update({
+    'step_type': raw_trajectory.step_type,
+    'reward': raw_trajectory.reward,
+    'discount': raw_trajectory.discount
+  })
+  
+  # remove batch size dimension
+  for key in observation:
+    observation[key] = tf.squeeze(observation[key], axis=0)
+  
+  return observation
 
 def main(_):
   gin.parse_config_files_and_bindings(
@@ -81,27 +111,35 @@ def main(_):
 
   raw_trajectory = next(dataset_iter)
 
-  observation = raw_trajectory.observation
-  observation.update({
-    'step_type': raw_trajectory.step_type,
-    'reward': raw_trajectory.reward,
-    'discount': raw_trajectory.discount
-  })
-  
-  # remove batch size dimension
-  for key in observation:
-    observation[key] = tf.squeeze(observation[key], axis=0)
-
   saved_policy = tf.saved_model.load(_MODEL_PATH.value)
   action_fn = saved_policy.signatures['action']
 
-  #output = action_fn(**observation)
-  
-  #explainer = shap.DeepExplainer()
-
+  observation = process_raw_trajectory(raw_trajectory)
   input_sig = get_input_signature(observation)
-  logging.info(input_sig)
-  logging.info(get_signature_total_size(input_sig))
+
+  def run_model(flat_input_array):
+    output = numpy.empty(flat_input_array.shape[0])
+    for index, flat_input in enumerate(flat_input_array):
+      input_dict = pack_flat_array_into_input(flat_input, input_sig)
+      model_output = action_fn(**input_dict).items()
+      # get the value of the first item as a numpy array
+      output[index] = list(model_output)[0][1].numpy()[0]
+    return output
+  
+  total_size = get_signature_total_size(input_sig)
+  flattened_input = flatten_input(observation, total_size)
+  flattened_input = numpy.expand_dims(flattened_input, axis=0)
+  dataset = numpy.empty((_NUM_EXAMPLES.value, total_size))
+  for i in range(0, _NUM_EXAMPLES.value):
+    raw_trajectory = next(dataset_iter)
+    observation = process_raw_trajectory(raw_trajectory)
+    flat_input = flatten_input(observation, total_size)
+    dataset[i] = flat_input
+  
+  explainer = shap.KernelExplainer(run_model, numpy.zeros((1, total_size)))
+  shap_values = explainer.shap_values(dataset, nsamples=1000)
+
+  numpy.savetxt(_OUTPUT_FILE.value, shap_values)
 
 if __name__ == '__main__':
   app.run(main)
