74a8347de799dbc4c89f8e8de96a78f1142dd2e4
Aiden Grossman <agrossman154@yahoo.com>
2022-08-17 07:57:07 +0000

    Fixed CI (probably)

--- compiler_opt/tools/feature_importance.py
+++ compiler_opt/tools/feature_importance.py
@@ -14,7 +14,6 @@
 # limitations under the License.
 """A tool for analyzing which features a model uses to make a decision."""
 
-from email.contentmanager import raw_data_manager
 from absl import app
 from absl import flags
 from absl import logging
@@ -29,34 +28,34 @@ import shap
 import numpy
 
 _DATA_PATH = flags.DEFINE_multi_string(
-  'data_path', [],
-  'Path to TFRecord file(s) containing trace data.')
-_MODEL_PATH = flags.DEFINE_string(
-  'model_path', '',
-  'Path to the model to explain')
+    'data_path', [], 'Path to TFRecord file(s) containing trace data.')
+_MODEL_PATH = flags.DEFINE_string('model_path', '',
+                                  'Path to the model to explain')
 _OUTPUT_FILE = flags.DEFINE_string(
-  'output_file', '',
-  'The path to the output file containing the SHAP values')
+    'output_file', '', 'The path to the output file containing the SHAP values')
 _NUM_EXAMPLES = flags.DEFINE_integer(
-  'num_examples', '',
-  'The number of examples to process from the trace')
+    'num_examples', '', 'The number of examples to process from the trace')
 _GIN_FILES = flags.DEFINE_multi_string(
     'gin_files', [], 'List of paths to gin configuration files.')
 _GIN_BINDINGS = flags.DEFINE_multi_string(
     'gin_bindings', [],
     'Gin bindings to override the values set in the config files.')
 
+
 def get_input_signature(example_input):
   input_signature = {}
-  for input in example_input:
-    input_signature[input] = (tf.shape(example_input[input]).numpy(), example_input[input].dtype)
+  for input_key in example_input:
+    input_signature[input_key] = (tf.shape(example_input[input_key]).numpy(),
+                                  example_input[input_key].dtype)
   return input_signature
+
 
 def get_signature_total_size(input_signature):
   total_size = 0
-  for input in input_signature:
-    total_size += numpy.prod(input_signature[input][0])
+  for input_key in input_signature:
+    total_size += numpy.prod(input_signature[input_key][0])
   return total_size
+
 
 def pack_flat_array_into_input(flat_array, signature_spec):
   output_input_dict = {}
@@ -65,32 +64,38 @@ def pack_flat_array_into_input(flat_array, signature_spec):
     part_size = numpy.prod(signature_spec[needed_input][0])
     needed_subset = flat_array[current_index:current_index + part_size]
     current_index += part_size
-    output_input_dict[needed_input] = tf.cast(tf.constant(needed_subset, shape=signature_spec[needed_input][0]), dtype=signature_spec[needed_input][1])
+    output_input_dict[needed_input] = tf.cast(
+        tf.constant(needed_subset, shape=signature_spec[needed_input][0]),
+        dtype=signature_spec[needed_input][1])
   return output_input_dict
 
-def flatten_input(input, array_size):
+
+def flatten_input(to_flatten, array_size):
   output_array = numpy.empty(array_size)
   input_index = 0
-  for input_key in input:
-    current_size = tf.size(input[input_key])
+  for input_key in to_flatten:
+    current_size = tf.size(to_flatten[input_key])
     end_index = input_index + current_size
-    output_array[input_index:end_index] = input[input_key].numpy().astype(numpy.float32)
+    output_array[input_index:end_index] = to_flatten[input_key].numpy().astype(
+        numpy.float32)
     input_index += current_size
   return output_array
+
 
 def process_raw_trajectory(raw_trajectory):
   observation = raw_trajectory.observation
   observation.update({
-    'step_type': raw_trajectory.step_type,
-    'reward': raw_trajectory.reward,
-    'discount': raw_trajectory.discount
+      'step_type': raw_trajectory.step_type,
+      'reward': raw_trajectory.reward,
+      'discount': raw_trajectory.discount
   })
-  
+
   # remove batch size dimension
   for key in observation:
     observation[key] = tf.squeeze(observation[key], axis=0)
-  
+
   return observation
+
 
 def main(_):
   gin.parse_config_files_and_bindings(
@@ -101,12 +106,12 @@ def main(_):
   time_step_spec, action_spec = problem_config.get_signature_spec()
 
   tfrecord_dataset_fn = data_reader.create_tfrecord_dataset_fn(
-    agent_name=constant.AgentName.BEHAVIORAL_CLONE,
-    time_step_spec=time_step_spec,
-    action_spec=action_spec,
-    batch_size=1,
-    train_sequence_length=1)
-  
+      agent_name=constant.AgentName.BEHAVIORAL_CLONE,
+      time_step_spec=time_step_spec,
+      action_spec=action_spec,
+      batch_size=1,
+      train_sequence_length=1)
+
   dataset_iter = iter(tfrecord_dataset_fn(_DATA_PATH.value).repeat())
 
   raw_trajectory = next(dataset_iter)
@@ -125,7 +130,7 @@ def main(_):
       # get the value of the first item as a numpy array
       output[index] = list(model_output)[0][1].numpy()[0]
     return output
-  
+
   total_size = get_signature_total_size(input_sig)
   flattened_input = flatten_input(observation, total_size)
   flattened_input = numpy.expand_dims(flattened_input, axis=0)
@@ -135,11 +140,12 @@ def main(_):
     observation = process_raw_trajectory(raw_trajectory)
     flat_input = flatten_input(observation, total_size)
     dataset[i] = flat_input
-  
+
   explainer = shap.KernelExplainer(run_model, numpy.zeros((1, total_size)))
   shap_values = explainer.shap_values(dataset, nsamples=1000)
 
   numpy.savetxt(_OUTPUT_FILE.value, shap_values)
+
 
 if __name__ == '__main__':
   app.run(main)
