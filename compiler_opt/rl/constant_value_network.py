1804d498b52fc86bd41c0538e730f40abd8afdff
Mircea Trofin <mtrofin@google.com>
2022-08-20 07:39:18 -0700

    Fix arguments-renamed warning introduced by TF 2.10

--- compiler_opt/rl/constant_value_network.py
+++ compiler_opt/rl/constant_value_network.py
@@ -45,9 +45,8 @@ class ConstantValueNetwork(network.Network):
 
     self._constant_output_val = constant_output_val
 
-  def call(self, observation, step_type=None, network_state=(), training=False):
+  def call(self, inputs, step_type=None, network_state=(), training=False):
     _ = (step_type, training)
-    shape = nest_utils.get_outer_array_shape(observation,
-                                             self._input_tensor_spec)
+    shape = nest_utils.get_outer_array_shape(inputs, self._input_tensor_spec)
     return tf.constant(
         self._constant_output_val, tf.float32, shape=shape), network_state
