bfa3390bd8e0a00940001c2dce777a5fd5d5f282
yundiqian <yundi@google.com>
2022-10-05 00:11:39 +0000

    lint

--- compiler_opt/rl/regalloc_priority/regalloc_priority_runner.py
+++ compiler_opt/rl/regalloc_priority/regalloc_priority_runner.py
@@ -1,3 +1,19 @@
+# coding=utf-8
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Module for collect data of regalloc priority prediction."""
+
 import gin
 import tensorflow as tf
 
@@ -6,7 +22,6 @@ import io
 import os
 import tempfile
 from typing import Dict, Optional, Tuple
-from absl import logging
 
 from google.protobuf import struct_pb2
 from compiler_opt.rl import compilation_runner
@@ -14,57 +29,59 @@ from compiler_opt.rl import compilation_runner
 
 @gin.configurable(module='runners')
 class RegAllocPriorityRunner(compilation_runner.CompilationRunner):
-    def _compile_fn(
-            self, file_paths: Tuple[str, ...], tf_policy_path: str, reward_only: bool,
-            cancellation_manager: Optional[
-                compilation_runner.WorkerCancellationManager]
-    ) -> Dict[str, Tuple[tf.train.SequenceExample, float]]:
+  """Class for collecting data for regalloc-priority-prediction."""
 
-        file_paths = file_paths[0].replace('.bc', '')
-        working_dir = tempfile.mkdtemp()
+  def _compile_fn(
+      self, file_paths: Tuple[str, ...], tf_policy_path: str, reward_only: bool,
+      cancellation_manager: Optional[
+          compilation_runner.WorkerCancellationManager]
+  ) -> Dict[str, Tuple[tf.train.SequenceExample, float]]:
 
-        log_path = os.path.join(working_dir, 'log')
-        output_native_path = os.path.join(working_dir, 'native')
+    file_paths = file_paths[0].replace('.bc', '')
+    working_dir = tempfile.mkdtemp()
 
-        result = {}
-        try:
-            command_line = []
-            if self._launcher_path:
-                command_line.append(self._launcher_path)
-            command_line.extend([self._clang_path] + [
-                                    '-c', file_paths, '-O3',
-                                    '-mllvm', '-regalloc-priority-training-log=' + log_path,
-                                    '-mllvm', '-regalloc-enable-priority-advisor=development',
-                                    '-o', output_native_path
-                                ])
+    log_path = os.path.join(working_dir, 'log')
+    output_native_path = os.path.join(working_dir, 'native')
 
-            if tf_policy_path:
-                command_line.extend(['-mllvm', '-regalloc-priority-model=' + tf_policy_path])
-            compilation_runner.start_cancellable_process(command_line,
-                                                         self._compilation_timeout,
-                                                         cancellation_manager)
+    result = {}
+    try:
+      command_line = []
+      if self._launcher_path:
+        command_line.append(self._launcher_path)
+      command_line.extend([self._clang_path] + [
+          '-c', file_paths, '-O3', '-mllvm', '-regalloc-priority-training-log='
+          + log_path, '-mllvm', '-regalloc-enable-priority-advisor=development',
+          '-o', output_native_path
+      ])
 
-            sequence_example = struct_pb2.Struct()
+      if tf_policy_path:
+        command_line.extend(
+            ['-mllvm', '-regalloc-priority-model=' + tf_policy_path])
+      compilation_runner.start_cancellable_process(command_line,
+                                                   self._compilation_timeout,
+                                                   cancellation_manager)
 
-            with io.open(log_path, 'rb') as f:
-                sequence_example.ParseFromString(f.read())
+      sequence_example = struct_pb2.Struct()
 
-            for key, value in sequence_example.fields.items():
-                e = tf.train.SequenceExample()
-                e.ParseFromString(base64.b64decode(value.string_value))
-                print(e)
-                if not e.HasField('feature_lists'):
-                    continue
-                r = (
-                    e.feature_lists.feature_list['reward'].feature[-1].float_list
-                        .value[0])
-                if reward_only:
-                    result[key] = (None, r)
-                else:
-                    del e.feature_lists.feature_list['reward']
-                    result[key] = (e, r)
+      with io.open(log_path, 'rb') as f:
+        sequence_example.ParseFromString(f.read())
 
-        finally:
-            tf.io.gfile.rmtree(working_dir)
+      for key, value in sequence_example.fields.items():
+        e = tf.train.SequenceExample()
+        e.ParseFromString(base64.b64decode(value.string_value))
+        print(e)
+        if not e.HasField('feature_lists'):
+          continue
+        r = (
+            e.feature_lists.feature_list['reward'].feature[-1].float_list
+            .value[0])
+        if reward_only:
+          result[key] = (None, r)
+        else:
+          del e.feature_lists.feature_list['reward']
+          result[key] = (e, r)
 
-        return result
+    finally:
+      tf.io.gfile.rmtree(working_dir)
+
+    return result
