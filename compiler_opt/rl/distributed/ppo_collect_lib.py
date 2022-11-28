b0890b4f9e7d35c5bf94d64db2c35ac6d8384767
Jacob Hegna <jacobhegna@gmail.com>
2022-11-28 18:45:53 +0000

    Add distributed PPO training.
    Distributed training works via four processes:
    
     - ppo_reverb_server.py
     - ppo_train.py
     - ppo_collect.py
     - ppo_eval.py
    
    ppo_train, ppo_collect, and ppo_eval communicate via ppo_reverb_server.
    The reverb server has two tables of data: one that holds the most recent
    model weights, and another that holds training data. The train script
    fetchs examples from reverb, trains, then uploads new model weights to
    reverb. The collect script fetches the latest model weights, collects
    training data, then uploads it to reverb. The eval script fetches the
    latest model weights and writes evaluation data to tensorboard.
    
    Training is distributed, so each of these processes can live on
    different servers, and you can have as many collect and eval jobs as you
    need.
    
    Distributed accelerator training is done via tf.distribute.Strategy. The
    ppo_train_lib.py script accepts a Strategy as input, and the
    ppo_train.py script uses the default strategy (cpu training). If you
    have access to TPUs, you can write an alternate ppo_train.py script that
    passes a TPUStrategy instead. Similarly, if your server has multiple
    GPUs, you can use a MirroredStrategy to train on all of them.

---
+++ compiler_opt/rl/distributed/ppo_collect_lib.py
@@ -0,0 +1,230 @@
+# coding=utf-8
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Collect experiences for PPO training"""
+
+import collections
+import os
+from typing import List, Optional
+import tempfile
+import functools
+
+from absl import logging
+
+import gin
+import reverb
+
+import tensorflow as tf
+import tensorflow.compat.v1 as tf_v1
+from tf_agents.experimental.distributed import reverb_variable_container
+from tf_agents.replay_buffers import reverb_utils
+
+from tf_agents.train import learner
+from tf_agents.train.utils import train_utils
+from tf_agents.utils import common
+
+from compiler_opt.rl import gin_external_configurables  # pylint: disable=unused-import
+from compiler_opt.rl import local_data_collector
+from compiler_opt.rl import corpus
+from compiler_opt.rl import data_reader
+from compiler_opt.rl import policy_saver
+from compiler_opt.rl import registry
+from compiler_opt.rl import agent_creators
+from compiler_opt.rl import constant
+from compiler_opt.rl import compilation_runner
+
+
+def _get_policy_bytes(agent):
+  """Recover the collect_policy bytes from a TF agent"""
+  policy_key = 'collect'
+  with tempfile.TemporaryDirectory() as tmpdirname:
+    saver = policy_saver.PolicySaver(policy_dict={
+        policy_key: agent.collect_policy,
+    })
+    saver.save(tmpdirname)
+    return policy_saver.Policy.from_filesystem(
+        os.path.join(tmpdirname, policy_key))
+
+
+class ReverbComplicationObserver(compilation_runner.CompilationResultObserver):
+  """Observer which sends compilation results to reverb"""
+
+  def __init__(self,
+               time_step_spec,
+               action_spec,
+               replay_buffer_server_address: str,
+               sequence_length: int,
+               initial_priority: float = 0.0):
+    self._observer = reverb_utils.ReverbTrajectorySequenceObserver(
+        reverb.Client(replay_buffer_server_address),
+        table_name=[
+            'training_table',
+        ],
+        sequence_length=sequence_length,
+        stride_length=sequence_length,
+        priority=initial_priority)
+
+    self._parser = data_reader.create_flat_sequence_example_dataset_fn(
+        agent_name=constant.AgentName.PPO_DISTRIBUTED,
+        time_step_spec=time_step_spec,
+        action_spec=action_spec)
+
+  def _is_actionable_result(
+      self, result: compilation_runner.CompilationResult) -> bool:
+    """Predicate which checks if the result contains valid experiences"""
+    return bool(result.keys)
+
+  def observe(self, result: compilation_runner.CompilationResult) -> None:
+    """Observe the compilation result.
+
+    Specifically, parses the serialized sequence examples and sends each
+    experience to reverb.
+    """
+    assert result.model_id is not None
+    if self._is_actionable_result(result):
+      # pylint: disable=protected-access
+      self._observer._priority = result.model_id
+      parsed = self._parser(result.serialized_sequence_examples)
+      for experience in parsed:
+        self._observer(experience)
+
+
+def collect(corpus_path: str, replay_buffer_server_address: str,
+            variable_container_server_address: str, num_workers: Optional[int],
+            worker_manager_class, sequence_length: int) -> None:
+  """Collects experience using a policy updated after every episode.
+
+  Args:
+    corpus_path: path to the corpus to collect from.
+    replay_buffer_server_address: address of the replay buffer in reverb.
+    variable_container_server_address: address of the variable container in
+      reverb.
+    num_workers: number of workers in the pool to collect from.
+    worker_manager_class: type of the worker manager to spawn the pool with.
+    sequence_length: sequence length of the examples.
+  """
+
+  # Set up the problem & initialize the agent
+  logging.info('Initializing the distributed PPO agent')
+  problem_config = registry.get_configuration()
+  time_step_spec, action_spec = problem_config.get_signature_spec()
+  agent = agent_creators.create_agent(
+      constant.AgentName.PPO_DISTRIBUTED, time_step_spec, action_spec,
+      problem_config.get_preprocessing_layer_creator())
+
+  # Initialize the reverb variable container
+  logging.info('Connecting to the reverb server')
+  train_step = tf_v1.train.get_or_create_global_step()
+  model_id = common.create_variable('model_id')
+  variables = {
+      reverb_variable_container.POLICY_KEY: agent.collect_policy.variables(),
+      reverb_variable_container.TRAIN_STEP_KEY: train_step,
+      'model_id': model_id
+  }
+  variable_container = reverb_variable_container.ReverbVariableContainer(
+      variable_container_server_address,
+      table_names=[reverb_variable_container.DEFAULT_TABLE])
+  variable_container.update(variables)
+
+  create_observer_fns = [
+      functools.partial(
+          ReverbComplicationObserver,
+          time_step_spec=time_step_spec,
+          action_spec=action_spec,
+          replay_buffer_server_address=replay_buffer_server_address,
+          sequence_length=sequence_length)
+  ]
+
+  # Setup the corpus
+  logging.info('Constructing tf.data pipeline and module corpus')
+  dataset_fn = data_reader.create_flat_sequence_example_dataset_fn(
+      agent_name=constant.AgentName.PPO_DISTRIBUTED,
+      time_step_spec=time_step_spec,
+      action_spec=action_spec)
+
+  def sequence_example_iterator_fn(seq_ex: List[str]):
+    return iter(dataset_fn(seq_ex).prefetch(tf.data.AUTOTUNE))
+
+  cps = corpus.Corpus(
+      data_path=corpus_path,
+      additional_flags=problem_config.flags_to_add(),
+      delete_flags=problem_config.flags_to_delete(),
+      replace_flags=problem_config.flags_to_replace())
+
+  # Run the experience collection loop.
+  logging.info('Constructing the worker pool')
+  with worker_manager_class(
+      worker_class=problem_config.get_runner_type(),
+      count=num_workers,
+      moving_average_decay_rate=1,
+      create_observer_fns=create_observer_fns) as worker_pool:
+
+    data_collector = local_data_collector.LocalDataCollector(
+        cps=cps,
+        num_modules=num_workers * 128 if num_workers else 512,
+        worker_pool=worker_pool,
+        parser=sequence_example_iterator_fn,
+        reward_stat_map=collections.defaultdict(lambda: None),
+        best_trajectory_repo=None)
+
+    logging.info('Starting collection loop')
+    while True:
+      logging.info('Collecting with model_id: %d at step %d',
+                   model_id.read_value(), train_step.numpy())
+      # Collect experiences which will be sent to reverb by each worker
+      dataset_iter, monitor_dict = data_collector.collect_data(
+          policy=_get_policy_bytes(agent), model_id=model_id.numpy())
+      del dataset_iter
+      del monitor_dict
+      # Fetch updated policy variables
+      variable_container.update(variables)
+
+
+@gin.configurable
+def run_collect(root_dir: str, corpus_path: str,
+                replay_buffer_server_address: str,
+                variable_container_server_address: str,
+                num_workers: Optional[int], worker_manager_class,
+                sequence_length: int):
+  """Collects experience using a policy updated after every episode.
+
+  Waits for a policy to be saved in root_dir before beginning collection.
+
+  Args:
+    root_dir: Directory where the trainer will save policies.
+    corpus_path: path to the corpus to collect from.
+    replay_buffer_server_address: address of the replay buffer in reverb.
+    variable_container_server_address: address of the variable container in
+      reverb.
+    num_workers: number of workers in the pool to collect from.
+    worker_manager_class: type of the worker manager to spawn the pool with.
+    sequence_length: sequence length of the examples.
+  """
+  # Wait for the collect policy to become available, as this signifies the
+  # trainer has come online
+  collect_policy_dir = os.path.join(root_dir, learner.POLICY_SAVED_MODEL_DIR,
+                                    learner.COLLECT_POLICY_SAVED_MODEL_DIR)
+  collect_policy = train_utils.wait_for_policy(
+      collect_policy_dir, load_specs_from_pbtxt=True)
+
+  # But we don't need it, because we will load parameters from the reverb server
+  del collect_policy
+
+  collect(
+      corpus_path=corpus_path,
+      replay_buffer_server_address=replay_buffer_server_address,
+      variable_container_server_address=variable_container_server_address,
+      num_workers=num_workers,
+      worker_manager_class=worker_manager_class,
+      sequence_length=sequence_length)
---
+++ compiler_opt/rl/distributed/ppo_train_lib.py
@@ -0,0 +1,174 @@
+# coding=utf-8
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Library to train a policy with PPO"""
+
+import time
+import os
+
+from absl import logging
+
+import gin
+import reverb
+import tensorflow.compat.v2 as tf
+
+from tf_agents.experimental.distributed import reverb_variable_container
+from tf_agents.replay_buffers import reverb_replay_buffer
+from tf_agents.train import learner as actor_learner
+from tf_agents.train import triggers
+from tf_agents.utils import common
+
+from compiler_opt.rl import gin_external_configurables  # pylint: disable=unused-import
+from compiler_opt.rl import constant
+from compiler_opt.rl import agent_creators
+from compiler_opt.rl import registry
+from compiler_opt.rl.distributed import learner as learner_lib
+
+_SHUFFLE_BUFFER_EPISODE_LEN = 3
+
+
+@gin.configurable
+def train(
+    root_dir: str,
+    strategy: tf.distribute.Strategy,
+    replay_buffer_server_address: str,
+    variable_container_server_address: str,
+    # Training params
+    per_replica_batch_size,
+    num_epochs: int,
+    num_iterations: int,
+    num_episodes_per_iteration: int,
+    sequence_length: int,
+    summary_interval: int):
+  """Trains a PPO agent."""
+  # Get the specs from the environment.
+  problem_config = registry.get_configuration()
+  time_step_spec, action_spec = problem_config.get_signature_spec()
+
+  # Create the agent.
+  with strategy.scope():
+    train_step = tf.compat.v1.train.get_or_create_global_step()
+    agent = agent_creators.create_agent(
+        constant.AgentName.PPO_DISTRIBUTED, time_step_spec, action_spec,
+        problem_config.get_preprocessing_layer_creator())
+    model_id = common.create_variable('model_id')
+    # The model_id should equal to the iteration number.
+    model_id.assign(0)
+    agent.initialize()
+
+  # Create the policy saver which saves the initial model now, then it
+  # periodically checkpoints the policy weigths.
+  saved_model_dir = os.path.join(root_dir, actor_learner.POLICY_SAVED_MODEL_DIR)
+  save_model_trigger = triggers.PolicySavedModelTrigger(
+      saved_model_dir, agent, train_step, interval=1000)
+
+  # Create the variable container.
+  variables = {
+      reverb_variable_container.POLICY_KEY: agent.collect_policy.variables(),
+      reverb_variable_container.TRAIN_STEP_KEY: train_step,
+      'model_id': model_id,
+  }
+  variable_container = reverb_variable_container.ReverbVariableContainer(
+      variable_container_server_address,
+      table_names=[reverb_variable_container.DEFAULT_TABLE])
+  variable_container.push(variables)
+
+  # Create the replay buffer.
+  reverb_replay_train = reverb_replay_buffer.ReverbReplayBuffer(
+      agent.collect_data_spec,
+      sequence_length=sequence_length,
+      table_name='training_table',
+      server_address=replay_buffer_server_address)
+
+  # Initialize the dataset.
+  def experience_dataset_fn():
+    get_dtype = lambda x: x.dtype  # pylint: disable=unnecessary-lambda-assignment
+    get_shape = lambda x: (None,) + x.shape  # pylint: disable=unnecessary-lambda-assignment
+
+    shapes = tf.nest.map_structure(get_shape, agent.collect_data_spec)
+    dtypes = tf.nest.map_structure(get_dtype, agent.collect_data_spec)
+
+    dataset = reverb.TrajectoryDataset(
+        server_address=replay_buffer_server_address,
+        table='training_table',
+        dtypes=dtypes,
+        shapes=shapes,
+        # Menger uses learner_iterations_per_call (256). Using 8 here instead
+        # because we do not need that much data in the buffer (they have to be
+        # filtered out for the next iteration anyways). The rule of thumb is
+        # 2-3x batch_size.
+        max_in_flight_samples_per_worker=8,
+        num_workers_per_iterator=-1,
+        max_samples_per_stream=-1,
+        rate_limiter_timeout_ms=-1,
+    )
+
+    def broadcast_info(info_traj):
+      # Assumes that the first element of traj is shaped
+      # (sequence_length, ...); and we extract this length.
+      info, traj = info_traj
+      first_elem = tf.nest.flatten(traj)[0]
+      length = first_elem.shape[0] or tf.shape(first_elem)[0]
+      info = tf.nest.map_structure(lambda t: tf.repeat(t, [length]), info)
+      return reverb.ReplaySample(info, traj)
+
+    dataset = dataset.map(broadcast_info)
+    return dataset
+
+  # Create the learner.
+  learning_triggers = [
+      save_model_trigger,
+      triggers.StepPerSecondLogTrigger(train_step, interval=100)
+  ]
+
+  def per_sequence_fn(sample):
+    # At this point, each sample data contains a sequence of trajectories.
+    data, info = sample.data, sample.info
+    data = agent.preprocess_sequence(data)
+    return data, info
+
+  learner = learner_lib.MLGOPPOLearner(
+      root_dir,
+      train_step,
+      model_id,
+      agent,
+      experience_dataset_fn,
+      sequence_length,
+      num_episodes_per_iteration=num_episodes_per_iteration,
+      minibatch_size=per_replica_batch_size,
+      shuffle_buffer_size=(_SHUFFLE_BUFFER_EPISODE_LEN * sequence_length),
+      triggers=learning_triggers,
+      summary_interval=summary_interval,
+      strategy=strategy,
+      num_epochs=num_epochs,
+      per_sequence_fn=per_sequence_fn,
+      allow_variable_length_episodes=False)
+
+  # Run the training loop.
+  for i in range(num_iterations):
+    step_val = train_step.numpy()
+    logging.info('Training. Iteration: %d', i)
+    start_time = time.time()
+    learner.wait_for_data()
+    data_wait_time = time.time() - start_time
+    logging.info('Data wait time sec: %s', data_wait_time)
+    loss_info = learner.run()
+    logging.info('Loss info: %s', str(loss_info))
+    num_steps = train_step.numpy() - step_val
+    run_time = time.time() - start_time
+    logging.info('Steps per sec: %s', num_steps / run_time)
+    logging.info('Pushing variables at model_id: %d', model_id.numpy())
+    variable_container.push(variables)
+    logging.info('clearing replay buffer')
+    reverb_replay_train.clear()
