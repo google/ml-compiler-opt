b5ec2bef12321605648fb6a7d520d3b7464a2708
Jacob Hegna <jacobhegna@gmail.com>
2022-12-12 16:16:40 -0600

    Add distributed training infrastructure. (#187)
    * Prepare for distributed training infrastructure.
    
    Distributed training has slightly different requirements than our
    current setup. This commit includes some small changes to prepare for
    the distributed training commit.
    
    Relevant changes:
     - add ability to observe compilation results within each worker (this
       will be used to efficiently send experiences to reverb). This is
       indended for users who need to observe compilations while they are in
       the distributed worker pool, rather than after they have been
       coalesced in the collection script.
    
     - add a sequence example dataset creator which doesn't batch examples
       (reverb does not handle batched examples).
    
     - slightly change the parsing logic to handle distributed PPO
       evaluation and training. In particular, distributed training
       requires that compute_value_and_advantage_in_train=False, which
       changes the policy_info spec that we require. Similarly, when
       evaluating the policy we convert the collect policy parameters stored
       on reverb to a greedy policy, which requires slightly changing how we
       parse data. Nothing changes in the "default" training case.
    
     - change the ConstantValueNetwork to determine its output shape
       dynamically based on the input shape.
    
    * Remove is_greedy flag from data_reader.py.
    
    * Remove value prediction parsing from data_reader.py.
    
    * Fix data_reader_test.py.
    
    * Remove spurious commas from data_reader.py.
    
    * Add distributed PPO training.
    
    Distributed training works via four processes:
    
     - ppo_reverb_server.py
     - ppo_train.py
     - ppo_collect.py
     - ppo_eval.py
    
    ppo_train, ppo_collect, and ppo_eval communicate via ppo_reverb_server.
    The reverb server has two tables of data: one that holds the most recent
    model weights, and another that holds training data. The train script
    fetchs examples from reverb, trains, then uploads new model weights to
    reverb. The collect script fetches the latest model weights, collects
    training data, then uploads it to reverb. The eval script fetches the
    latest model weights and writes evaluation data to tensorboard.
    
    Training is distributed, so each of these processes can live on
    different servers, and you can have as many collect and eval jobs as you
    need.
    
    Distributed accelerator training is done via tf.distribute.Strategy. The
    ppo_train_lib.py script accepts a Strategy as input, and the
    ppo_train.py script uses the default strategy (cpu training). If you
    have access to TPUs, you can write an alternate ppo_train.py script that
    passes a TPUStrategy instead. Similarly, if your server has multiple
    GPUs, you can use a MirroredStrategy to train on all of them.
    
    * Fix type: Complication -> Compilation.
    
    * Slight changes to the gin configs.
    
    * Add value prediction to ppo_collect_lib.py.
    
    * Add tf_agents import to ppo_collect_lib.py.

---
+++ compiler_opt/rl/distributed/ppo_eval_lib.py
@@ -0,0 +1,193 @@
+# coding=utf-8
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Evaluate the current policy stored in reverb"""
+
+import tempfile
+import collections
+import os
+import time
+from typing import List, Optional
+
+from absl import logging
+
+import tensorflow as tf
+import tensorflow.compat.v1 as tf_v1
+from tf_agents.experimental.distributed import reverb_variable_container
+from tf_agents.train import learner
+from tf_agents.train.utils import train_utils
+from tf_agents.utils import common
+
+from compiler_opt.rl import data_reader
+from compiler_opt.rl import local_data_collector
+from compiler_opt.rl import gin_external_configurables  # pylint: disable=unused-import
+from compiler_opt.rl import corpus
+from compiler_opt.rl import constant
+from compiler_opt.rl import agent_creators
+from compiler_opt.rl import registry
+from compiler_opt.rl import policy_saver
+from compiler_opt.rl import data_collector
+
+
+def evaluate(root_dir: str, corpus_path: str,
+             variable_container_server_address: str, num_workers: Optional[int],
+             worker_manager_class):
+  """Evaluate a given policy on the given corpus.
+
+  Args:
+    root_dir: path to write tensorboard summaries.
+    corpus_path: path to the corpus to collect from.
+    variable_container_server_address: address of the variable container in
+      reverb.
+    num_workers: number of workers in the pool to collect from.
+    worker_manager_class: type of the worker manager to spawn the pool with.
+  """
+
+  # Set up the problem & initialize the agent
+  logging.info('Initializing the distributed PPO agent')
+  problem_config = registry.get_configuration()
+  time_step_spec, action_spec = problem_config.get_signature_spec()
+  agent = agent_creators.create_agent(
+      constant.AgentName.PPO_DISTRIBUTED, time_step_spec, action_spec,
+      problem_config.get_preprocessing_layer_creator())
+
+  #policy = greedy_policy.GreedyPolicy(agent.policy)
+  policy = agent.collect_policy
+  policy_dict = {'policy': policy}
+
+  # Initialize the reverb variable container
+  logging.info('Connecting to the reverb server')
+  train_step = tf_v1.train.get_or_create_global_step()
+  model_id = common.create_variable('model_id')
+  variables = {
+      reverb_variable_container.POLICY_KEY: policy.variables(),
+      reverb_variable_container.TRAIN_STEP_KEY: train_step,
+      'model_id': model_id
+  }
+  variable_container = reverb_variable_container.ReverbVariableContainer(
+      variable_container_server_address,
+      table_names=[reverb_variable_container.DEFAULT_TABLE])
+  variable_container.update(variables)
+
+  # Setup the corpus
+  logging.info('Constructing tf.data pipeline and module corpus')
+  dataset_fn = data_reader.create_flat_sequence_example_dataset_fn(
+      agent_name=constant.AgentName.PPO_DISTRIBUTED,
+      time_step_spec=time_step_spec,
+      action_spec=action_spec)
+
+  def sequence_example_iterator_fn(seq_ex: List[str]):
+    return iter(dataset_fn(seq_ex).prefetch(tf.data.AUTOTUNE))
+
+  cps = corpus.Corpus(
+      data_path=corpus_path,
+      additional_flags=problem_config.flags_to_add(),
+      delete_flags=problem_config.flags_to_delete(),
+      replace_flags=problem_config.flags_to_replace())
+
+  summary_writer = tf.summary.create_file_writer(
+      root_dir, flush_millis=5 * 1000)
+  summary_writer.set_as_default()
+
+  data_action_mean = tf.keras.metrics.Mean()
+  data_reward_mean = tf.keras.metrics.Mean()
+
+  # Run the experience collection loop.
+  with worker_manager_class(
+      worker_class=problem_config.get_runner_type(),
+      count=num_workers,
+      moving_average_decay_rate=1) as worker_pool:
+    logging.info('constructed pool')
+    collector = local_data_collector.LocalDataCollector(
+        cps=cps,
+        num_modules=128,
+        worker_pool=worker_pool,
+        parser=sequence_example_iterator_fn,
+        reward_stat_map=collections.defaultdict(lambda: None),
+        best_trajectory_repo=None)
+    logging.info('constructed data_collector')
+    last_step = -1
+    rewards = []
+    actions = []
+    while True:
+      with tempfile.TemporaryDirectory() as tmpdirname:
+        saver = policy_saver.PolicySaver(policy_dict=policy_dict)
+        saver.save(tmpdirname)
+        policy_bytes = policy_saver.Policy.from_filesystem(
+            os.path.join(tmpdirname, 'policy'))
+      dataset_iter, monitor_dict = collector.collect_data(
+          policy=policy_bytes, model_id=model_id.numpy())
+      del monitor_dict
+
+      for experience in dataset_iter:
+        is_action = ~experience.is_boundary()
+
+        # pylint: disable=not-callable
+        data_action_mean.update_state(
+            experience.action, sample_weight=is_action)
+        # pylint: disable=not-callable
+        data_reward_mean.update_state(
+            experience.reward, sample_weight=is_action)
+
+        if is_action:
+          rewards.append(experience.reward)
+          actions.append(experience.action)
+
+      if last_step < train_step.numpy():
+        last_step = train_step.numpy()
+        with tf.name_scope('default/'):
+          tf.summary.scalar(
+              name='data_action_mean',
+              # pylint: disable=not-callable
+              data=data_action_mean.result(),
+              step=train_step)
+          tf.summary.scalar(
+              name='data_reward_mean',
+              # pylint: disable=not-callable
+              data=data_reward_mean.result(),
+              step=train_step)
+          tf.summary.scalar(
+              name='num_eval_experiences', data=len(rewards), step=train_step)
+        tf.summary.histogram(name='reward', data=rewards, step=train_step)
+        tf.summary.histogram(name='action', data=actions, step=train_step)
+        with tf.name_scope('reward_distribution/'):
+          reward_dist = data_collector.build_distribution_monitor(rewards)
+          for k, v in reward_dist.items():
+            tf.summary.scalar(name=k, data=v, step=train_step)
+        data_action_mean.reset_state()
+        data_reward_mean.reset_state()
+        rewards = []
+        actions = []
+
+      variable_container.update(variables)
+      logging.info('Evaluating with policy at step: %d', train_step.numpy())
+      time.sleep(20)
+
+
+def run_evaluate(root_dir: str, corpus_path: str,
+                 variable_container_server_address: str,
+                 num_workers: Optional[int], worker_manager_class):
+  """Wait for the collect policy to be ready and run collect job."""
+  # Wait for the collect policy to become available, then load it.
+  policy_dir = os.path.join(root_dir, learner.POLICY_SAVED_MODEL_DIR,
+                            learner.COLLECT_POLICY_SAVED_MODEL_DIR)
+  policy = train_utils.wait_for_policy(policy_dir, load_specs_from_pbtxt=True)
+  del policy
+
+  evaluate(
+      root_dir=root_dir,
+      corpus_path=corpus_path,
+      variable_container_server_address=variable_container_server_address,
+      num_workers=num_workers,
+      worker_manager_class=worker_manager_class)
---
+++ compiler_opt/rl/distributed/ppo_reverb_server.py
@@ -0,0 +1,56 @@
+# coding=utf-8
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Main binary to launch a stand alone Reverb RB server."""
+
+from absl import app
+from absl import flags
+from absl import logging
+
+import gin
+
+from compiler_opt.rl.distributed import ppo_reverb_server_lib
+from compiler_opt.rl import registry  # pylint: disable=unused-import
+from compiler_opt.rl import agent_creators  # pylint: disable=unused-import
+
+flags.DEFINE_string('root_dir', None,
+                    'Root directory for writing logs/summaries/checkpoints.')
+flags.DEFINE_integer('replay_buffer_capacity', 1000000,
+                     'Capacity of the replay buffer table.')
+flags.DEFINE_integer('port', None, 'Port to start the server on.')
+flags.DEFINE_multi_string('gin_files', [],
+                          'List of paths to gin configuration files.')
+flags.DEFINE_multi_string(
+    'gin_bindings', [],
+    'Gin bindings to override the values set in the config files.')
+
+FLAGS = flags.FLAGS
+
+
+def main(_):
+  logging.set_verbosity(logging.INFO)
+
+  gin.parse_config_files_and_bindings(
+      FLAGS.gin_files, bindings=FLAGS.gin_bindings, skip_unknown=False)
+  logging.info(gin.config_str())
+
+  ppo_reverb_server_lib.run_reverb_server(
+      FLAGS.root_dir,
+      port=FLAGS.port,
+      replay_buffer_capacity=FLAGS.replay_buffer_capacity)
+
+
+if __name__ == '__main__':
+  flags.mark_flags_as_required(['root_dir', 'port'])
+  app.run(main)
