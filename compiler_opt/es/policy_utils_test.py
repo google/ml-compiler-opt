485b8b1a2e70a336152bcd75f06c41414b3b134e
Abena Laast <abenalaast@google.com>
2023-07-24 22:23:06 +0000

    Restructure value tests and rename variables for clarity

--- compiler_opt/es/policy_utils_test.py
+++ compiler_opt/es/policy_utils_test.py
@@ -92,6 +92,12 @@ class ConfigTest(absltest.TestCase):
 
 class VectorTest(absltest.TestCase):
 
+  expected_variable_shapes = [(71, 64), (64), (64, 64), (64), (64, 64), (64),
+                              (64, 64), (64), (64, 2), (2)]
+  expected_length_of_a_perturbation = sum(
+      np.prod(shape) for shape in expected_variable_shapes)
+  params = np.arange(expected_length_of_a_perturbation, dtype=np.float32)
+
   def test_set_vectorized_parameters_for_policy(self):
     # create a policy
     problem_config = registry.get_configuration(implementation=InliningConfig)
@@ -115,41 +121,37 @@ class VectorTest(absltest.TestCase):
         time_step_spec=time_step_spec,
         action_spec=action_spec,
         actor_network=actor_network)
-    saver = policy_saver.PolicySaver({'policy': policy})
 
     # save the policy
+    saver = policy_saver.PolicySaver({'policy': policy})
     testing_path = self.create_tempdir()
     policy_save_path = os.path.join(testing_path, 'temp_output/policy')
     saver.save(policy_save_path)
 
     # set the values of the policy variables
-    length_of_a_perturbation = 17218
-    params = np.arange(length_of_a_perturbation, dtype=np.float32)
-    policy_utils.set_vectorized_parameters_for_policy(policy, params)
-    expected_variable_shapes = [(71, 64), (64), (64, 64), (64), (64, 64), (64),
-                                (64, 64), (64), (64, 2), (2)]
+    policy_utils.set_vectorized_parameters_for_policy(policy, VectorTest.params)
     # iterate through variables and check their shapes and values
-    idx = 0
+    expected_values = [*VectorTest.params]
     for i, variable in enumerate(policy.variables()):  # pylint: disable=not-callable
-      self.assertEqual(variable.shape, expected_variable_shapes[i])
-      nums = variable.numpy().flatten()
-      for num in nums:
-        if idx != num:
-          raise AssertionError(f'values at index {idx} do not match')
-        idx += 1
+      self.assertEqual(variable.shape, VectorTest.expected_variable_shapes[i])
+      variable_values = variable.numpy().flatten()
+      np.testing.assert_array_almost_equal(
+          expected_values[:len(variable_values)], variable_values)
+      expected_values = expected_values[len(variable_values):]
+    self.assertEmpty(expected_values)
 
     # get saved model to test a loaded policy
     sm = tf.saved_model.load(policy_save_path + '/policy')
     self.assertNotIsInstance(sm, tf_policy.TFPolicy)
-    policy_utils.set_vectorized_parameters_for_policy(sm, params)
-    idx = 0
+    policy_utils.set_vectorized_parameters_for_policy(sm, VectorTest.params)
+    expected_values = [*VectorTest.params]
     for i, variable in enumerate(sm.model_variables):
-      self.assertEqual(variable.shape, expected_variable_shapes[i])
-      nums = variable.numpy().flatten()
-      for num in nums:
-        if idx != num:
-          raise AssertionError(f'values at index {idx} do not match')
-        idx += 1
+      self.assertEqual(variable.shape, VectorTest.expected_variable_shapes[i])
+      variable_values = variable.numpy().flatten()
+      np.testing.assert_array_almost_equal(
+          expected_values[:len(variable_values)], variable_values)
+      expected_values = expected_values[len(variable_values):]
+    self.assertEmpty(expected_values)
 
   def test_get_vectorized_parameters_from_policy(self):
     # create a policy
@@ -174,28 +176,26 @@ class VectorTest(absltest.TestCase):
         time_step_spec=time_step_spec,
         action_spec=action_spec,
         actor_network=actor_network)
-    saver = policy_saver.PolicySaver({'policy': policy})
 
     # save the policy
+    saver = policy_saver.PolicySaver({'policy': policy})
     testing_path = self.create_tempdir()
     policy_save_path = os.path.join(testing_path, 'temp_output/policy')
     saver.save(policy_save_path)
 
-    length_of_a_perturbation = 17218
-    params = np.arange(length_of_a_perturbation, dtype=np.float32)
     # functionality verified in previous test
-    policy_utils.set_vectorized_parameters_for_policy(policy, params)
+    policy_utils.set_vectorized_parameters_for_policy(policy, VectorTest.params)
     # vectorize and check if the outcome is the same as the start
     output = policy_utils.get_vectorized_parameters_from_policy(policy)
-    np.testing.assert_array_almost_equal(output, params)
+    np.testing.assert_array_almost_equal(output, VectorTest.params)
 
     # get saved model to test a loaded policy
     sm = tf.saved_model.load(policy_save_path + '/policy')
     self.assertNotIsInstance(sm, tf_policy.TFPolicy)
-    policy_utils.set_vectorized_parameters_for_policy(sm, params)
+    policy_utils.set_vectorized_parameters_for_policy(sm, VectorTest.params)
     # vectorize and check if the outcome is the same as the start
     output = policy_utils.get_vectorized_parameters_from_policy(sm)
-    np.testing.assert_array_almost_equal(output, params)
+    np.testing.assert_array_almost_equal(output, VectorTest.params)
 
   def test_tfpolicy_and_loaded_policy_produce_same_variable_order(self):
     # create a policy
@@ -220,17 +220,15 @@ class VectorTest(absltest.TestCase):
         time_step_spec=time_step_spec,
         action_spec=action_spec,
         actor_network=actor_network)
-    saver = policy_saver.PolicySaver({'policy': policy})
 
     # save the policy
+    saver = policy_saver.PolicySaver({'policy': policy})
     testing_path = self.create_tempdir()
     policy_save_path = os.path.join(testing_path, 'temp_output/policy')
     saver.save(policy_save_path)
 
-    length_of_a_perturbation = 17218
-    params = np.arange(length_of_a_perturbation, dtype=np.float32)
     # set the values of the variables
-    policy_utils.set_vectorized_parameters_for_policy(policy, params)
+    policy_utils.set_vectorized_parameters_for_policy(policy, VectorTest.params)
     # save the changes
     saver.save(policy_save_path)
     # vectorize the tfpolicy
