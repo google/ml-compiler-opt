74b7605bd1c982a31576cbacd9232b4294456868
Abena Laast <abenalaast@google.com>
2023-07-25 22:31:56 +0000

    Use os.path.join to form paths

--- compiler_opt/es/policy_utils_test.py
+++ compiler_opt/es/policy_utils_test.py
@@ -29,14 +29,15 @@ from compiler_opt.rl.regalloc import config as regalloc_config
 from compiler_opt.rl.regalloc import RegallocEvictionConfig, regalloc_network
 
 
-# TODO(abenalaast): Issue #280
 class ConfigTest(absltest.TestCase):
 
+  # TODO(abenalaast): Issue #280
   def test_inlining_config(self):
     problem_config = registry.get_configuration(implementation=InliningConfig)
     time_step_spec, action_spec = problem_config.get_signature_spec()
+    quantile_file_dir = os.path.join('compiler_opt', 'rl', 'inlining', 'vocab')
     creator = inlining_config.get_observation_processing_layer_creator(
-        quantile_file_dir='compiler_opt/rl/inlining/vocab/',
+        quantile_file_dir=quantile_file_dir,
         with_sqrt=False,
         with_z_score_normalization=False)
     layers = tf.nest.map_structure(creator, time_step_spec.observation)
@@ -60,12 +61,14 @@ class ConfigTest(absltest.TestCase):
         policy._actor_network,  # pylint: disable=protected-access
         actor_distribution_network.ActorDistributionNetwork)
 
+  # TODO(abenalaast): Issue #280
   def test_regalloc_config(self):
     problem_config = registry.get_configuration(
         implementation=RegallocEvictionConfig)
     time_step_spec, action_spec = problem_config.get_signature_spec()
+    quantile_file_dir = os.path.join('compiler_opt', 'rl', 'regalloc', 'vocab')
     creator = regalloc_config.get_observation_processing_layer_creator(
-        quantile_file_dir='compiler_opt/rl/regalloc/vocab',
+        quantile_file_dir=quantile_file_dir,
         with_sqrt=False,
         with_z_score_normalization=False)
     layers = tf.nest.map_structure(creator, time_step_spec.observation)
@@ -97,13 +100,16 @@ class VectorTest(absltest.TestCase):
   expected_length_of_a_perturbation = sum(
       np.prod(shape) for shape in expected_variable_shapes)
   params = np.arange(expected_length_of_a_perturbation, dtype=np.float32)
+  POLICY_NAME = 'test_policy_name'
 
+  # TODO(abenalaast): Issue #280
   def test_set_vectorized_parameters_for_policy(self):
     # create a policy
     problem_config = registry.get_configuration(implementation=InliningConfig)
     time_step_spec, action_spec = problem_config.get_signature_spec()
+    quantile_file_dir = os.path.join('compiler_opt', 'rl', 'inlining', 'vocab')
     creator = inlining_config.get_observation_processing_layer_creator(
-        quantile_file_dir='compiler_opt/rl/inlining/vocab/',
+        quantile_file_dir=quantile_file_dir,
         with_sqrt=False,
         with_z_score_normalization=False)
     layers = tf.nest.map_structure(creator, time_step_spec.observation)
@@ -123,14 +129,15 @@ class VectorTest(absltest.TestCase):
         actor_network=actor_network)
 
     # save the policy
-    saver = policy_saver.PolicySaver({'policy': policy})
+    saver = policy_saver.PolicySaver({VectorTest.POLICY_NAME: policy})
     testing_path = self.create_tempdir()
-    policy_save_path = os.path.join(testing_path, 'temp_output/policy')
+    policy_save_path = os.path.join(testing_path, 'temp_output', 'policy')
     saver.save(policy_save_path)
 
     # set the values of the policy variables
     policy_utils.set_vectorized_parameters_for_policy(policy, VectorTest.params)
     # iterate through variables and check their shapes and values
+    # deep copy params in order to destructively iterate over values
     expected_values = [*VectorTest.params]
     for i, variable in enumerate(policy.variables()):  # pylint: disable=not-callable
       self.assertEqual(variable.shape, VectorTest.expected_variable_shapes[i])
@@ -138,12 +145,15 @@ class VectorTest(absltest.TestCase):
       np.testing.assert_array_almost_equal(
           expected_values[:len(variable_values)], variable_values)
       expected_values = expected_values[len(variable_values):]
+    # all values in the copy should have been removed at this point
     self.assertEmpty(expected_values)
 
     # get saved model to test a loaded policy
-    sm = tf.saved_model.load(policy_save_path + '/policy')
+    load_path = os.path.join(policy_save_path, VectorTest.POLICY_NAME)
+    sm = tf.saved_model.load(load_path)
     self.assertNotIsInstance(sm, tf_policy.TFPolicy)
     policy_utils.set_vectorized_parameters_for_policy(sm, VectorTest.params)
+    # deep copy params in order to destructively iterate over values
     expected_values = [*VectorTest.params]
     for i, variable in enumerate(sm.model_variables):
       self.assertEqual(variable.shape, VectorTest.expected_variable_shapes[i])
@@ -151,14 +161,17 @@ class VectorTest(absltest.TestCase):
       np.testing.assert_array_almost_equal(
           expected_values[:len(variable_values)], variable_values)
       expected_values = expected_values[len(variable_values):]
+    # all values in the copy should have been removed at this point
     self.assertEmpty(expected_values)
 
+  # TODO(abenalaast): Issue #280
   def test_get_vectorized_parameters_from_policy(self):
     # create a policy
     problem_config = registry.get_configuration(implementation=InliningConfig)
     time_step_spec, action_spec = problem_config.get_signature_spec()
+    quantile_file_dir = os.path.join('compiler_opt', 'rl', 'inlining', 'vocab')
     creator = inlining_config.get_observation_processing_layer_creator(
-        quantile_file_dir='compiler_opt/rl/inlining/vocab/',
+        quantile_file_dir=quantile_file_dir,
         with_sqrt=False,
         with_z_score_normalization=False)
     layers = tf.nest.map_structure(creator, time_step_spec.observation)
@@ -178,9 +191,9 @@ class VectorTest(absltest.TestCase):
         actor_network=actor_network)
 
     # save the policy
-    saver = policy_saver.PolicySaver({'policy': policy})
+    saver = policy_saver.PolicySaver({VectorTest.POLICY_NAME: policy})
     testing_path = self.create_tempdir()
-    policy_save_path = os.path.join(testing_path, 'temp_output/policy')
+    policy_save_path = os.path.join(testing_path, 'temp_output', 'policy')
     saver.save(policy_save_path)
 
     # functionality verified in previous test
@@ -190,19 +203,22 @@ class VectorTest(absltest.TestCase):
     np.testing.assert_array_almost_equal(output, VectorTest.params)
 
     # get saved model to test a loaded policy
-    sm = tf.saved_model.load(policy_save_path + '/policy')
+    load_path = os.path.join(policy_save_path, VectorTest.POLICY_NAME)
+    sm = tf.saved_model.load(load_path)
     self.assertNotIsInstance(sm, tf_policy.TFPolicy)
     policy_utils.set_vectorized_parameters_for_policy(sm, VectorTest.params)
     # vectorize and check if the outcome is the same as the start
     output = policy_utils.get_vectorized_parameters_from_policy(sm)
     np.testing.assert_array_almost_equal(output, VectorTest.params)
 
+  # TODO(abenalaast): Issue #280
   def test_tfpolicy_and_loaded_policy_produce_same_variable_order(self):
     # create a policy
     problem_config = registry.get_configuration(implementation=InliningConfig)
     time_step_spec, action_spec = problem_config.get_signature_spec()
+    quantile_file_dir = os.path.join('compiler_opt', 'rl', 'inlining', 'vocab')
     creator = inlining_config.get_observation_processing_layer_creator(
-        quantile_file_dir='compiler_opt/rl/inlining/vocab/',
+        quantile_file_dir=quantile_file_dir,
         with_sqrt=False,
         with_z_score_normalization=False)
     layers = tf.nest.map_structure(creator, time_step_spec.observation)
@@ -222,9 +238,9 @@ class VectorTest(absltest.TestCase):
         actor_network=actor_network)
 
     # save the policy
-    saver = policy_saver.PolicySaver({'policy': policy})
+    saver = policy_saver.PolicySaver({VectorTest.POLICY_NAME: policy})
     testing_path = self.create_tempdir()
-    policy_save_path = os.path.join(testing_path, 'temp_output/policy')
+    policy_save_path = os.path.join(testing_path, 'temp_output', 'policy')
     saver.save(policy_save_path)
 
     # set the values of the variables
@@ -235,7 +251,8 @@ class VectorTest(absltest.TestCase):
     tf_params = policy_utils.get_vectorized_parameters_from_policy(policy)
 
     # get loaded policy
-    sm = tf.saved_model.load(policy_save_path + '/policy')
+    load_path = os.path.join(policy_save_path, VectorTest.POLICY_NAME)
+    sm = tf.saved_model.load(load_path)
     # vectorize the loaded policy
     loaded_params = policy_utils.get_vectorized_parameters_from_policy(sm)
 
