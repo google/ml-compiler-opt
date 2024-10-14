25b4e6ba47bbc8a6680c0581d1bbd8ae0fbbf1e3
Teodor V. Marinov <tvmarinov@google.com>
2024-10-14 18:42:15 +0000

    Initial ExplorationWorker commit. ExplorationWorker implements the interactive clang compilation together with exploration for a given module. This commit adds the class with its compile_module function which will compile the module with a given compiler policy.

--- compiler_opt/rl/generate_bc_trajectories_test.py
+++ compiler_opt/rl/generate_bc_trajectories_test.py
@@ -15,6 +15,7 @@
 """Tests for compiler_opt.rl.generate_bc_trajectories."""
 
 from typing import List
+from unittest import mock
 
 import numpy as np
 import tensorflow as tf
@@ -22,7 +23,11 @@ import tensorflow_probability as tfp
 from tf_agents.trajectories import policy_step
 from tf_agents.trajectories import time_step
 
+from google.protobuf import text_format
+
 from compiler_opt.rl import generate_bc_trajectories
+from compiler_opt.rl import env
+from compiler_opt.rl import env_test
 
 _eps = 1e-5
 
@@ -152,3 +157,191 @@ class ExplorationWithPolicyTest(tf.test.TestCase):
     for state in _get_state_list():
       _ = explore_with_policy.get_advice(state)[0]
     self.assertEqual(1, explore_with_policy.explore_step)
+
+
+class AddToFeatureListsTest(tf.test.TestCase):
+
+  def test_add_int_feature(self):
+    sequence_example_text = """
+      feature_lists {
+        feature_list {
+          key: "feature_0"
+          value {
+            feature { int64_list { value: 1 } }
+            feature { int64_list { value: 2 } }
+          }
+        }
+        feature_list {
+          key: "feature_1"
+          value {
+            feature { int64_list { value: 3 } }
+          }
+        }
+      }"""
+    sequence_example_comp = text_format.Parse(sequence_example_text,
+                                              tf.train.SequenceExample())
+
+    sequence_example = tf.train.SequenceExample()
+    generate_bc_trajectories.add_int_feature(
+        sequence_example=sequence_example,
+        feature_value=1,
+        feature_name='feature_0')
+    generate_bc_trajectories.add_int_feature(
+        sequence_example=sequence_example,
+        feature_value=2,
+        feature_name='feature_0')
+    generate_bc_trajectories.add_int_feature(
+        sequence_example=sequence_example,
+        feature_value=3,
+        feature_name='feature_1')
+
+    self.assertEqual(sequence_example, sequence_example_comp)
+
+  def test_add_float_feature(self):
+    sequence_example_text = """
+      feature_lists {
+        feature_list {
+          key: "feature_0"
+          value {
+            feature { float_list { value: .1 } }
+            feature { float_list { value: .2 } }
+          }
+        }
+        feature_list {
+          key: "feature_1"
+          value {
+            feature { float_list { value: .3 } }
+          }
+        }
+      }"""
+    sequence_example_comp = text_format.Parse(sequence_example_text,
+                                              tf.train.SequenceExample())
+
+    sequence_example = tf.train.SequenceExample()
+    generate_bc_trajectories.add_float_feature(
+        sequence_example=sequence_example,
+        feature_value=.1,
+        feature_name='feature_0')
+    generate_bc_trajectories.add_float_feature(
+        sequence_example=sequence_example,
+        feature_value=.2,
+        feature_name='feature_0')
+    generate_bc_trajectories.add_float_feature(
+        sequence_example=sequence_example,
+        feature_value=.3,
+        feature_name='feature_1')
+
+    self.assertEqual(sequence_example, sequence_example_comp)
+
+  def test_add_string_feature(self):
+    sequence_example_text = """
+      feature_lists {
+        feature_list {
+          key: "feature_0"
+          value {
+            feature { bytes_list { value: "1" } }
+            feature { bytes_list { value: "2" } }
+          }
+        }
+        feature_list {
+          key: "feature_1"
+          value {
+            feature { bytes_list { value: "3" } }
+          }
+        }
+      }"""
+    sequence_example_comp = text_format.Parse(sequence_example_text,
+                                              tf.train.SequenceExample())
+
+    sequence_example = tf.train.SequenceExample()
+    generate_bc_trajectories.add_string_feature(
+        sequence_example=sequence_example,
+        feature_value='1',
+        feature_name='feature_0')
+    generate_bc_trajectories.add_string_feature(
+        sequence_example=sequence_example,
+        feature_value='2',
+        feature_name='feature_0')
+    generate_bc_trajectories.add_string_feature(
+        sequence_example=sequence_example,
+        feature_value='3',
+        feature_name='feature_1')
+
+    self.assertEqual(sequence_example, sequence_example_comp)
+
+
+class ExplorationWorkerTest(tf.test.TestCase):
+  # pylint: disable=protected-access
+  @mock.patch('subprocess.Popen')
+  def test_create_timestep(self, mock_popen):
+    mock_popen.side_effect = env_test.mock_interactive_clang
+
+    def create_timestep_comp(step_type, reward, obs):
+      timestep_comp = time_step.TimeStep(
+          step_type=tf.convert_to_tensor([step_type],
+                                         dtype=tf.int32,
+                                         name='step_type'),
+          reward=tf.convert_to_tensor([reward], dtype=tf.float32,
+                                      name='reward'),
+          discount=tf.convert_to_tensor([0.0],
+                                        dtype=tf.float32,
+                                        name='discount'),
+          observation=obs,
+      )
+      return timestep_comp
+
+    test_env = env.MLGOEnvironmentBase(
+        clang_path=env_test._CLANG_PATH,
+        task_type=env_test.MockTask,
+        obs_spec={},
+        action_spec={},
+    )
+
+    exploration_worker = generate_bc_trajectories.ExplorationWorker(
+        loaded_module_spec=env_test._MOCK_MODULE,
+        clang_path=env_test._CLANG_PATH,
+        mlgo_task=env_test.MockTask,
+        reward_key='default',
+    )
+
+    curr_step_obs = test_env.reset(env_test._MOCK_MODULE)
+    timestep = exploration_worker._create_timestep(curr_step_obs)
+    timestep_comp = create_timestep_comp(0, 0., curr_step_obs.obs)
+    self.assertEqual(timestep, timestep_comp)
+
+    for step_itr in range(env_test._NUM_STEPS - 1):
+      del step_itr
+      curr_step_obs = test_env.step(np.array([1], dtype=np.int64))
+      timestep = exploration_worker._create_timestep(curr_step_obs)
+      timestep_comp = create_timestep_comp(1, 0., curr_step_obs.obs)
+      self.assertEqual(timestep, timestep_comp)
+
+    curr_step_obs = test_env.step(np.array([1], dtype=np.int64))
+    timestep = exploration_worker._create_timestep(curr_step_obs)
+    timestep_comp = create_timestep_comp(2, 47., curr_step_obs.obs)
+    self.assertEqual(timestep, timestep_comp)
+
+  @mock.patch('subprocess.Popen')
+  def test_compile_module(self, mock_popen):
+    mock_popen.side_effect = env_test.mock_interactive_clang
+
+    seq_example_comp = tf.train.SequenceExample()
+    for i in range(10):
+      generate_bc_trajectories.add_int_feature(seq_example_comp, i,
+                                               'times_called')
+      generate_bc_trajectories.add_string_feature(seq_example_comp, 'module',
+                                                  'module_name')
+      generate_bc_trajectories.add_float_feature(seq_example_comp, 47.0,
+                                                 'reward')
+      generate_bc_trajectories.add_int_feature(seq_example_comp, np.mod(i, 5),
+                                               'action')
+
+    exploration_worker = generate_bc_trajectories.ExplorationWorker(
+        loaded_module_spec=env_test._MOCK_MODULE,
+        clang_path=env_test._CLANG_PATH,
+        mlgo_task=env_test.MockTask,
+        reward_key='default',
+    )
+
+    seq_example = exploration_worker.compile_module(_policy)
+    self.assertEqual(seq_example, seq_example_comp)
