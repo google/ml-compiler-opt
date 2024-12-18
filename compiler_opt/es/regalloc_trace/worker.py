2cf5d1f85308ec96ede5a679f2abc129bb92606a
Aiden Grossman <aidengrossman@google.com>
2024-12-18 17:31:50 +0000

    [DO NOT SUBMIT] Add regalloc trace worker

---
+++ compiler_opt/es/regalloc_trace/worker.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Worker for regalloc using trace-based cost modeling."""
+
+import gin
+
+@gin.configurable
+class RegallocTraceWorker(worker.Worker):
+  """A worker that produces rewards for a given regalloc policy."""
+
+  def __init__():
+    pass
+  
+  def compile_module():
+    pass
+
+  def build_corpus():
+    pass
+
+  def evaluate_corpus():
+    pass
+
+  def compile_corpus_and_evaluate():
+    pass
