3106e499175a66db1b0313da0a68badb8b8c12ef
Aiden Grossman <aidengrossman@google.com>
2025-03-11 00:35:13 +0000

    [𝘀𝗽𝗿] changes introduced through rebase
    Created using spr 1.3.4
    
    [skip ci]

--- compiler_opt/es/regalloc_trace/regalloc_trace_worker.py
+++ compiler_opt/es/regalloc_trace/regalloc_trace_worker.py
@@ -64,9 +64,8 @@ class RegallocTraceWorker(worker.Worker):
                    copy_corpus_locally_path: str | None) -> None:
     """Makes a local copy of the corpus if requested.
 
-    This function makes a local copy of the corpus if requested by copying the
-    remote corpus to a user-specified directory. If no local corpus copy
-    location is specified, it simply returns the existing path.
+    This function makes a local copy of the corpus by copying the remote
+    corpus to a user-specified directory.
 
     Args:
       corpus_path: The path to the remote corpus.
