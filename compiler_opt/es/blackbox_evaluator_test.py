875676efbabd987c593d8aa0c7ac47ab3e79afa0
Aiden Grossman <aidengrossman@google.com>
2025-05-20 13:23:38 -0700

    Make create_corpus_for_testing use module_spec command lines
    This patch makes create_corpus_for_testing use the command lines from
    individual modle_specs passed in as elements rather than using the same
    command line for all the modules. This is needed for testing the group
    functions library.
    
    Reviewers: svkeerthy, mtrofin
    
    Reviewed By: mtrofin
    
    Pull Request: https://github.com/google/ml-compiler-opt/pull/505

--- compiler_opt/es/blackbox_evaluator_test.py
+++ compiler_opt/es/blackbox_evaluator_test.py
@@ -63,7 +63,9 @@ class BlackboxEvaluatorTests(absltest.TestCase):
       perturbations = [b'00', b'01', b'10']
       test_corpus = corpus.create_corpus_for_testing(
           location=self.create_tempdir().full_path,
-          elements=[corpus.ModuleSpec(name='name1', size=1)])
+          elements=[
+              corpus.ModuleSpec(name='name1', size=1, command_line=('-cc1',))
+          ])
       evaluator = blackbox_evaluator.TraceBlackboxEvaluator(
           test_corpus, blackbox_optimizers.EstimatorType.FORWARD_FD,
           'fake_bb_trace_path', 'fake_function_index_path')
@@ -82,7 +84,9 @@ class BlackboxEvaluatorTests(absltest.TestCase):
         worker_kwargs={}) as pool:
       test_corpus = corpus.create_corpus_for_testing(
           location=self.create_tempdir().full_path,
-          elements=[corpus.ModuleSpec(name='name1', size=1)])
+          elements=[
+              corpus.ModuleSpec(name='name1', size=1, command_line=('-cc1',))
+          ])
       evaluator = blackbox_evaluator.TraceBlackboxEvaluator(
           test_corpus, blackbox_optimizers.EstimatorType.FORWARD_FD,
           'fake_bb_trace_path', 'fake_function_index_path')
@@ -100,7 +104,9 @@ class BlackboxEvaluatorTests(absltest.TestCase):
     results = [f1, f2]
     test_corpus = corpus.create_corpus_for_testing(
         location=self.create_tempdir().full_path,
-        elements=[corpus.ModuleSpec(name='name1', size=1)])
+        elements=[
+            corpus.ModuleSpec(name='name1', size=1, command_line=('-cc1',))
+        ])
     evaluator = blackbox_evaluator.TraceBlackboxEvaluator(
         test_corpus, blackbox_optimizers.EstimatorType.FORWARD_FD,
         'fake_bb_trace_path', 'fake_function_index_path')
