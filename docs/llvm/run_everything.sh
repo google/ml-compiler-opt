8113e816cfc65ad0ced5e9e1e6dd87ac134aa102
Mircea Trofin <mtrofin@google.com>
2025-12-17 22:08:08 -0800

    fix run_everything.sh

--- docs/llvm/run_everything.sh
+++ docs/llvm/run_everything.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-./init.sh
-./build_clang_for_training.sh
-./build_clang_for_corpus.sh
-./extract_corpus.sh
-./generate_default_trace.sh
-./generate_vocab.sh
-./train_with_es.sh
+SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
+
+SCRIPT_DIR="$(cd -- "$SCRIPT_DIR" && pwd)"
+if [[ -z "$SCRIPT_DIR" ]] ; then
+    exit 1
+fi
+
+"${SCRIPT_DIR}/init.sh"
+"${SCRIPT_DIR}/build_clang_for_training.sh"
+"${SCRIPT_DIR}/build_clang_for_corpus.sh"
+"${SCRIPT_DIR}/extract_corpus.sh"
+"${SCRIPT_DIR}/generate_default_trace.sh"
+"${SCRIPT_DIR}/generate_vocab.sh"
+"${SCRIPT_DIR}/train_with_es.sh"
